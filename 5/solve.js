46b08e622ef007b561dc9d574c799f88d899873d
Jake (CDIO) Ord <68291857+ordyboii@users.noreply.github.com>
2023-12-10 17:09:47 +0000

    so far

--- 5/solve.js
+++ 5/solve.js
@@ -1,34 +1,10 @@
 const fs = require("fs");
 
-const input = fs.readFileSync('4/input.txt', 'utf-8');
-const example = fs.readFileSync('4/example.txt', 'utf-8');
+const input = fs.readFileSync('5/input.txt', 'utf-8');
+const example = fs.readFileSync('5/example.txt', 'utf-8');
 
-const lines = input.split('\n');
+const maps = example.split('\n\n');
+// const seeds = maps[0].split(':')[1].trim().split(' ').map(num => parseInt(num));
 
-let p1 = 0;
-const p2 = lines.map(() => 1);
-
-for (let i = 0; i < lines.length; i++) {
-  const winnNums = lines[i].split('|')[0].split(':')[1].trim().split(' ').map(num => parseInt(num));
-  const drawnNums = lines[i].split('|')[1].split(' ').filter(num => num !== '').map(num => parseInt(num));
-
-  let total = 0;
-  let matches = 0;
-
-  for (const num of drawnNums) {
-    if (winnNums.includes(num)) {
-      total = total === 0 ? 1 : total * 2;
-      matches += 1;
-    }
-  }
-
-  p1 += total;
-
-  for (let j = 0; j < matches; j++) {
-    p2[i + j + 1] += p2[i];
-  }
-}
-
-console.log('Part one:', p1);
-console.log('Part two:', p2.reduce((a, b) => a + b));
+console.log(maps[0]);
 
